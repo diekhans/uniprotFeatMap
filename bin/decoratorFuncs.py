42e982f0b0121017fe2c65aff1a551eb807efc92
Mark Diekhans <markd@ucsc.edu>
2023-04-18 14:24:21 -0700

    basic alignment working

--- bin/decoratorFuncs.py
+++ bin/decoratorFuncs.py
@@ -4,40 +4,47 @@ Library of common functions and other definitions for building domain decorators
 
 from os import path as osp
 
-def _isOutOfDate(doneFile, dependencies):
-    modTime = osp.getmtime(doneFile)
-    for depend in dependencies:
-        if osp.getmtime(depend) > modTime:
-            return True
-    return False
-
 def getDoneFile(target):
     "get path to done file corresponding to target"
     return osp.normpath(target) + ".done"
 
+def _isOutOfDate(doneFile, depends):
+    modTime = osp.getmtime(doneFile)
+    for depend in depends:
+        if osp.getmtime(depend) > modTime:
+            return True
+    return False
+
+def _isDoneOutOfDate(doneFile, doneDepends):
+    return _isOutOfDate(doneFile, [getDoneFile(d) for d in doneDepends])
+
 class runIfNotDone:
-    """Cheap make-like hack. checks if a file target.done exists.  If
-    dependencies are listed, the check that target.done is newer.  If target
-    is a directory, create a file target.done beside it, not inside of it.
+    """Context manager for a Cheap make-like hack. Checks if a flag file to
+    see if something is done. The with context returns a True if you need to
+    do something, false otherwise.  Checks if target exists. If there are
+    dependencies, the check that target.done is newer.  depends are check
+    for actually files, doneDepends are check for done files
+
+    If the body succeeds, a file target.done is created. If target is a
+    directory, create a file target.done beside it, not inside of it.
+
     """
 
-    def _isOutOfDate(self, doneFile, dependencies):
-        if isinstance(dependencies, str):
-            dependencies = [dependencies]
-        modTime = osp.getmtime(doneFile)
-        for depend in dependencies:
-            if osp.getmtime(depend) > modTime:
-                return True
-        return False
+    def __init__(self, target, *, depends=None, doneDepends=None):
+        "depends can be a string filename or list"
+        if isinstance(depends, str):
+            depends = [depends]
+        if isinstance(doneDepends, str):
+            doneDepends = [doneDepends]
 
-    def __init__(self, target, dependencies=None):
-        "dependencies can be a string filename or list"
         self.doneFile = getDoneFile(target)
         self.outOfDate = ((not osp.exists(self.doneFile)) or
-                          ((dependencies is not None) and _isOutOfDate(dependencies)))
+                          ((depends is not None) and _isOutOfDate(self.doneFile, depends)) or
+                          ((doneDepends is not None) and _isDoneOutOfDate(self.doneFile, doneDepends)))
+
 
     def __enter__(self):
-        return self if self.outOfDate else None
+        return self.outOfDate
 
     def __exit__(self, exc_type, exc_value, traceback):
         if (exc_type is None) and self.outOfDate:
