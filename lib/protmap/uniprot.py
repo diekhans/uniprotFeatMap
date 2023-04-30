0b0acdc7620fac74c21d04856583a96ba4364f26
Mark Diekhans <markd@ucsc.edu>
2023-04-29 19:52:33 -0700

    added uniprotIsoCanonicalSelect

--- lib/protmap/uniprot.py
+++ lib/protmap/uniprot.py
@@ -3,6 +3,8 @@ from protmap import dropVersion
 
 def splitMetaList(val):
     """ split strings like: ENST00000369413.8|ENST00000528909.1"""
+    if val == "":
+        return []
     return val.split('|')
 
 def splitDropVersion(idents):
@@ -18,10 +20,27 @@ class UniprotMeta:
         # index by gene and transcript ids
         self.df['ensemblGeneAccs'] = self.df.ensemblGene.apply(splitDropVersion)
         self.df['ensemblTransAccs'] = self.df.ensemblTrans.apply(splitDropVersion)
+        self.df['isoIds'] = self.df.isoIds.apply(splitMetaList)
         self.byGeneAccDf = self.df.explode('ensemblGeneAccs')
         self.byGeneAccDf.rename(columns={'ensemblGeneAccs': 'ensemblGeneAcc'}, inplace=True)
         self.byTranscriptAccDf = self.df.explode('ensemblTransAccs')
         self.byTranscriptAccDf.rename(columns={'ensemblTransAccs': 'ensemblTransAcc'}, inplace=True)
+
+        # with only one isoform: acc == mainIsoAcc, and isoIds are empty
+        # for multiple isoforms: mainIsoAcc is canonical and isoIds are other isoforms.
+        # Build an index by isoId which is
+        self.df['allIsoIds'] = self.df.apply(lambda row: row.isoIds + [row.mainIsoAcc], axis=1)
+        self.byIsoId = self.df.explode('allIsoIds')
+        self.byIsoId.rename(columns={'allIsoIds': 'isoId'}, inplace=True)
+
+    def getByAcc(self, acc):
+        return self.df[self.df.acc == acc].iloc[0]
+
+    def getByIsoId(self, isoId):
+        try:
+            return self.byIsoId[self.byIsoId.isoId == isoId].iloc[0]
+        except IndexError as ex:
+            raise IndexError(f"isoId not found {isoId}") from ex
 
     def getTransMeta(self, transAcc):
         protMetas = self.byTranscriptAccDf[self.byTranscriptAccDf.ensemblTransAcc == transAcc]
