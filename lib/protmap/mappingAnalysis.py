932ff981e7facde42266edf7bf48ef4679b2194b
Mark Diekhans <markd@ucsc.edu>
2023-12-07 13:09:00 -0800

    fix deletion traigle location being on wrong side with negative strand genes

--- lib/protmap/mappingAnalysis.py
+++ lib/protmap/mappingAnalysis.py
@@ -5,12 +5,10 @@ from collections import namedtuple
 
 class FeatureIndel(namedtuple("FeatureIndel",
                               ("isFeatDel", "length",
-                               "qStart", "qEnd",
                                "tStart", "tEnd",
                                ))):
-    """Describes an INDEL.  For featDel, length is
-    bases deleted, for inst it is length of insert.
-    ranges can be zero length"""
+    """Describes an INDEL.  For featDel, length is bases deleted, for inst it
+    is length of insert.  ranges can be zero length."""
     __slots__ = ()
 
 def _isIntron(transPsl, tStart, tEnd):
@@ -26,28 +24,24 @@ def _isIntron(transPsl, tStart, tEnd):
 
 def _analyzeStart(annotPsl):
     if annotPsl.qStart > 0:
-        # unaligned before first block
-        yield FeatureIndel(True, annotPsl.qStart,
-                           0, annotPsl.blocks[0].qStart,
-                           annotPsl.blocks[0].tStart, annotPsl.blocks[0].tStart)
+        # unaligned before start of query
+        tPos = annotPsl.blocks[0].tStart if annotPsl.qStrand == '+' else annotPsl.blocks[-1].tEnd
+        yield FeatureIndel(True, annotPsl.qStart, tPos, tPos)
 
 def _analyzeEnd(annotPsl):
     if annotPsl.qEnd < annotPsl.qSize:
         # unaligned after last block
-        yield FeatureIndel(True, annotPsl.qSize - annotPsl.qEnd,
-                           annotPsl.blocks[-1].qEnd, annotPsl.qSize,
-                           annotPsl.blocks[-1].tEnd, annotPsl.blocks[-1].tEnd)
+        tPos = annotPsl.blocks[-1].tEnd if annotPsl.qStrand == '+' else annotPsl.blocks[0].tStart
+        yield FeatureIndel(True, annotPsl.qSize - annotPsl.qEnd, tPos, tPos)
 
 def _analyzeBlock(transPsl, annotPsl, iBlk):
     blk = annotPsl.blocks[iBlk]
     nextBlk = annotPsl.blocks[iBlk + 1]
     if blk.qEnd < nextBlk.qStart:
         yield FeatureIndel(True, (nextBlk.qStart - blk.qEnd),
-                           blk.qEnd, nextBlk.qStart,
                            blk.tEnd, nextBlk.tStart)
     if (blk.tEnd < nextBlk.tStart) and (not _isIntron(transPsl, blk.tEnd, nextBlk.tStart)):
         yield FeatureIndel(False, (nextBlk.tStart - blk.tEnd),
-                           blk.qEnd, nextBlk.qStart,
                            blk.tEnd, nextBlk.tStart)
 
 def _analyzeBlocks(transPsl, annotPsl):
@@ -64,4 +58,15 @@ def analyzeFeatureMapping(transPsl, annotPsl):
     unmapped regions of feature or insertions in
     feature that don't correspond to introns in
     transcripts."""
-    return tuple(_featureIndelGen(transPsl, annotPsl))
+    #return tuple(_featureIndelGen(transPsl, annotPsl))
+
+    v = tuple(_featureIndelGen(transPsl, annotPsl))
+
+    if (transPsl.qName == "ENST00000697272.1") and (len(v) > 0):
+        import pipettor#fixme
+        print("*", str(annotPsl))
+        print(pipettor.runout(["pslFmt"], stdin=pipettor.DataWriter(str(annotPsl) + '\n')))
+        for x in v:
+            print(x)
+        print()
+    return v#tmp
