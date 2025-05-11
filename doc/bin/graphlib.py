d733061719d88bb143422c6386881ac9d88d45c2
Mark Diekhans <markd@ucsc.edu>
2025-05-11 16:35:25 -0700

    split out graphviz stuff

---
+++ doc/bin/graphlib.py
@@ -0,0 +1,93 @@
+import os.path as osp
+import graphviz as gv  # pip install graphviz
+
+def check_output_file_type(parser, outfile):
+    if _get_output_format(outfile) not in gv.FORMATS:
+        fmts = ", ".join(gv.FORMATS)
+        parser.error(f"output file must end in one of the format extensions: {fmts}: '{outfile}'")
+
+def _get_output_format(outfile):
+    return osp.splitext(outfile)[1][1:]
+
+def _get_output_base(outfile):
+    return osp.splitext(outfile)[0]
+
+class GraphBuilder:
+    def __init__(self, title, outfile, size=None):
+        self.outfile = outfile
+        fontname = "helvetica"
+        graph_attr = {"fontname": fontname}
+        if size is not None:
+            graph_attr["size"] = size
+        node_attr = {"fontname": fontname}
+        edge_attr = {"fontname": fontname}
+        self.graph = gv.Digraph(
+            title,
+            format=_get_output_format(self.outfile),
+            graph_attr=graph_attr,
+            node_attr=node_attr,
+            edge_attr=edge_attr)
+        self.data_files = set()
+
+    def ensure_optional_data_file(self, name):
+        "create an data file node, if it does not already exist"
+        if name not in self.data_files:
+            self.graph.node(name, label=name,
+                            shape="cylinder",
+                            style="dashed,filled",
+                            fillcolor="yellow",
+                            fontcolor="black")
+            self.data_files.add(name)
+
+    def ensure_data_file(self, name):
+        "create an data file node, if it does not already exist"
+        if name not in self.data_files:
+            self.graph.node(name, label=name,
+                            shape="cylinder",
+                            style="filled",
+                            fillcolor="cyan",
+                            fontcolor="black")
+            self.data_files.add(name)
+
+    def ensure_data_files(self, names):
+        for name in names:
+            self.ensure_data_file(name)
+
+    def add_task(self, name, inputs=(), outputs=()):
+        "create a task node, inputs and outputs are other node names"
+        label = name.replace(" ", "\\n")
+        self.graph.node(name, label=label,
+                        shape="box",
+                        style="filled",
+                        fillcolor="maroon",
+                        fontcolor="white")
+        for n in inputs:
+            self.ensure_data_file(n)
+            self.graph.edge(n, name)
+        for n in outputs:
+            self.ensure_data_file(n)
+            self.graph.edge(name, n)
+
+    def add_program(self, name, inputs=(), outputs=()):
+        self.graph.node(name,
+                        label=name,
+                        shape="trapezium",
+                        style="filled",
+                        fillcolor="darkgreen",
+                        fontcolor="white")
+        for n in inputs:
+            self.ensure_data_file(n)
+            self.graph.edge(n, name)
+        for n in outputs:
+            self.ensure_data_file(n)
+            self.graph.edge(name, n)
+
+    def cluster(self, name):
+        invis = {"style": "invis"}
+        return self.graph.subgraph(name="cluster_" + name, graph_attr=invis)
+
+    def group(self, name):
+        return self.graph.subgraph(name="relevant")
+
+    def render(self, keep=False):
+        self.graph.render(_get_output_base(self.outfile), cleanup=not keep)
